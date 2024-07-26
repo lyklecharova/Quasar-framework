774188ffc860015e6cb2bf3a1881e51be97a7ac6
lyklecharova <lklecharova9@gmail.com>
2024-07-26 11:27:13 +0300

    Update IndexPage.vue
    Created functionalities:
    increase, decrease and reset for counter

--- quasar-counter/src/pages/IndexPage.vue
+++ quasar-counter/src/pages/IndexPage.vue
@@ -1,15 +1,54 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="flex flex-center text-white">
+    <div class="row">
+      <q-input
+        v-model="data.name"
+        input-class="text-center text-h5 text-white"
+        color="teal"
+        placeholder="Counter"
+        filled
+      />
+    </div>
+    <div class="row full-width items-center">
+      <div class="col text-center">
+        <q-btn @click="decreaseCounter" icon="remove" size="xl" round />
+      </div>
+      <div class="col text-center text-h2">{{ data.counter }}</div>
+      <div class="col text-center">
+        <q-btn @click="increaseCounter" icon="add" size="xl" round />
+      </div>
+    </div>
+    <div class="row">
+      <q-btn  @click="resetCounter" icon="restart_alt" size="xl" round />
+    </div>
   </q-page>
 </template>
 
+<style scoped>
+.q-page {
+  max-width: 700px;
+  margin: 0 auto;
+}
+</style>
 <script setup>
-defineOptions({
-  name: 'IndexPage'
+/* Shift + Alt + A - comment code */
+/* imports */
+import { reactive } from "vue";
+/* data */
+const data = reactive({
+  counter: 0,
+  name: "",
 });
+/* counter methods */
+const increaseCounter = () => {
+  data.counter++;
+};
+const decreaseCounter = () => {
+    if(data.counter >0)
+  data.counter--;
+};
+const resetCounter = () => {
+    data.counter = 0;
+};
+
 </script>
